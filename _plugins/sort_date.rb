f2b21ef5d3506b1fd61d98d410990a45b146d3ac
Rey Abolofia <rey.abolofia@stitchfix.com>
2021-03-13 02:44:56 -0800

    Be deteriministic for multiple problems on a single date.

--- _plugins/sort_date.rb
+++ _plugins/sort_date.rb
@@ -1,6 +1,10 @@
 module SortDateFilter
   def sort_date(input)
-    input.filter { |p| !p.data['completed'].nil? }.sort_by { |p| p.data['completed'] }
+    input.filter { |p|
+      !p.data['completed'].nil?
+    }.sort_by { |p|
+      [p.data['completed'], p.data['title']]
+    }
   end
 end
 
