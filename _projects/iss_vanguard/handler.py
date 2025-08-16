934b80b07d731311a4314b47d37c4da7527d796a
Rey Abolofia <purple4reina@gmail.com>
2025-08-15 23:46:28 -0700

    Handle base64 encoded payloads.

--- _projects/iss_vanguard/handler.py
+++ _projects/iss_vanguard/handler.py
@@ -1,10 +1,14 @@
+import base64
 import json
 
 from probability import calculate_probability
 
-def handle(request):
+def handle(request, context=None):
     print("Received request:", request)
-    inputs = json.loads(request.get('body') or '{}')
+    body = request.get('body', '{}')
+    if request.get('isBase64Encoded'):
+        body = base64.b64decode(body).decode('utf-8')
+    inputs = json.loads(body)
     result = calculate_probability(inputs)
     return {
             'statusCode': 200,
