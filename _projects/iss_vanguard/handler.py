8c7b0316779af60c35e0e3282f975c2e597bb82a
Rey Abolofia <purple4reina@gmail.com>
2025-08-15 23:56:33 -0700

    Handle cors.

--- _projects/iss_vanguard/handler.py
+++ _projects/iss_vanguard/handler.py
@@ -5,11 +5,22 @@ from probability import calculate_probability
 
 def handle(request, context=None):
     print("Received request:", request)
+    if request['requestContext']['http']['method'] == 'OPTIONS':
+        return {
+            'statusCode': 204,
+            'headers': {
+                'Access-Control-Allow-Origin': 'https://purple4reina.github.io',
+                'Access-Control-Allow-Methods': 'OPTIONS, POST',
+                'Access-Control-Allow-Headers': 'Content-Type',
+            }
+        }
+
     body = request.get('body', '{}')
     if request.get('isBase64Encoded'):
         body = base64.b64decode(body).decode('utf-8')
     inputs = json.loads(body)
     result = calculate_probability(inputs)
+
     return {
             'statusCode': 200,
             'body': json.dumps(result),
